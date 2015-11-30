5135b113f4fd4134e403a9f0abea7e6d72b34184
benjaminkron111 <benjamin@appleOfiMac.local>
2015-11-30 14:19:43 +0100

    init commit

---
+++ WhirlyMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhirlyMapDemo
+//
+//  Created by benjamin on 30/11/15.
+//  Copyright © 2015 Benjamin Kron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhirlyMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhirlyMapDemo
+//
+//  Created by benjamin on 30/11/15.
+//  Copyright © 2015 Benjamin Kron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
